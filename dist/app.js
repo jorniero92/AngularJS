ea97e4c22022a00fd0c3de3e1e15f32af0dfadcc
jorge nieto <jorge.nieto@babel.es>
2016-03-21 13:59:08 +0100

    separacion de pestañas y va a cada vista HTML de cada uno

--- dist/app.js
+++ dist/app.js
@@ -31593,7 +31593,22 @@ function ngViewFillContentFactory($compile, $controller, $route) {
 
 ;//defino el modul0 "moviedb" e 
 //inicializo las dependencias con --> []
-angular.module("moviedb", ['ngRoute']);
+angular.module("moviedb", ['ngRoute']).config(
+    ["$routeProvider", function($routeProvider) {
+
+        //configuro las URLs de la aplicacion
+        $routeProvider.when('/movies/', {
+        	templateUrl: 'views/MoviesList.html'
+        }).when('/series/', {
+        	templateUrl: 'views/SeriesList.html'
+        }).when('/people/', {
+        	templateUrl: 'views/PeopleList.html'
+        }).when('/', {
+        	redirectTo: '/movies'
+        }).otherwise({
+        	templateUrl: 'views/404.html'
+        });
+    }]);
 ;angular.module("moviedb").controller("AppController", ["$scope", function($scope) {
     /* Inicializar el $scope */
     $scope.model = {
--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,18 @@
 //defino el modul0 "moviedb" e 
 //inicializo las dependencias con --> []
-angular.module("moviedb", ['ngRoute']);
+angular.module("moviedb", ['ngRoute']).config(
+    ["$routeProvider", function($routeProvider) {
+
+        //configuro las URLs de la aplicacion
+        $routeProvider.when('/movies/', {
+        	templateUrl: 'views/MoviesList.html'
+        }).when('/series/', {
+        	templateUrl: 'views/SeriesList.html'
+        }).when('/people/', {
+        	templateUrl: 'views/PeopleList.html'
+        }).when('/', {
+        	redirectTo: '/movies'
+        }).otherwise({
+        	templateUrl: 'views/404.html'
+        });
+    }]);
