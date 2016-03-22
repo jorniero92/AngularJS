3cc71fea7c20c50f38fbdb79e1788f7f525ac282
jorge nieto <jorge.nieto@babel.es>
2016-03-22 10:27:16 +0100

    crear una tabla en movies

--- dist/app.js
+++ dist/app.js
@@ -31684,5 +31684,32 @@ angular.module("moviedb").controller("MenuController", ["$scope","$location", fu
     });
 }]);
 ;angular.module("moviedb").controller("MoviesListController", ["$scope", function($scope) {
-	$scope.name = "Joseba";
+
+    /* Scope model init */
+    $scope.model = [{
+        "title": "Deadpool",
+        "poster_url": "https://image.tmdb.org/t/p/w185/inVq3FRqcYIRl2la8iZikYYxFNR.jpg",
+        "rating": 7.2,
+        "release_date": "2016-02-19"
+    }, {
+        "title": "The Hunger Games: Mockingjay - Part 2",
+        "poster_url": "https://image.tmdb.org/t/p/w185/nN4cEJMHJHbJBsp3vvvhtNWLGqg.jpg",
+        "rating": 6.8,
+        "release_date": "2015-11-27"
+    }, {
+        "title": "Batman v Superman: Dawn of Justice",
+        "poster_url": "https://image.tmdb.org/t/p/w185/6bCplVkhowCjTHXWv49UjRPn0eK.jpg",
+        "rating": 5.1,
+        "release_date": "2016-03-23"
+    }, {
+        "title": "Mad Max: Fury Road",
+        "poster_url": "https://image.tmdb.org/t/p/w185/kqjL17yufvn9OVLyXYpvtyrFfak.jpg",
+        "rating": 7.4,
+        "release_date": "2015-05-15"
+    }, {
+        "title": "Zootopia",
+        "poster_url": "https://image.tmdb.org/t/p/w185/sM33SANp9z6rXW8Itn7NnG1GOEs.jpg",
+        "rating": 7.4,
+        "release_date": "2016-02-12"
+    }];
 }]);
