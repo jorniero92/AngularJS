9efa25d407b666e4c9a40e77e577f2e2da1d9a7c
jorge nieto <jorge.nieto@babel.es>
2016-03-22 11:58:48 +0100

    Separacion del texto del codigo

--- dist/app.js
+++ dist/app.js
@@ -31683,36 +31683,12 @@ angular.module("moviedb").controller("MenuController", ["$scope","$location", fu
         $scope.model.selectedItem = $location.path();
     });
 }]);
-;angular.module("moviedb").controller("MoviesListController", ["$scope", function($scope) {
+;angular.module("moviedb").controller("MoviesListController", 
+    ["$scope", "MovieService", function($scope, MovieService) {
 
-    $scope.uiState = 'ideal';
+    $scope.uiState = 'blank';
     /* Scope model init */
-    $scope.model = [{
-        "title": "Deadpool",
-        "poster_url": "https://image.tmdb.org/t/p/w185/inVq3FRqcYIRl2la8iZikYYxFNR.jpg",
-        "rating": 7.2,
-        "release_date": "2016-02-19"
-    }, {
-        "title": "The Hunger Games: Mockingjay - Part 2",
-        "poster_url": "https://image.tmdb.org/t/p/w185/nN4cEJMHJHbJBsp3vvvhtNWLGqg.jpg",
-        "rating": 6.8,
-        "release_date": "2015-11-27"
-    }, {
-        "title": "Batman v Superman: Dawn of Justice",
-        "poster_url": "https://image.tmdb.org/t/p/w185/6bCplVkhowCjTHXWv49UjRPn0eK.jpg",
-        "rating": 5.1,
-        "release_date": "2016-03-23"
-    }, {
-        "title": "Mad Max: Fury Road",
-        "poster_url": "https://image.tmdb.org/t/p/w185/kqjL17yufvn9OVLyXYpvtyrFfak.jpg",
-        "rating": 7.4,
-        "release_date": "2015-05-15"
-    }, {
-        "title": "Zootopia",
-        "poster_url": "https://image.tmdb.org/t/p/w185/sM33SANp9z6rXW8Itn7NnG1GOEs.jpg",
-        "rating": 7.4,
-        "release_date": "2016-02-12"
-    }];
+    $scope.model = [];
 
     /*Scope Wathec*/
     $scope.$watch("model", function(newValue, oldValue) {
@@ -31722,4 +31698,38 @@ angular.module("moviedb").controller("MenuController", ["$scope","$location", fu
             $scope.uiState = 'ideal';
         }
     });
+
+    /* controller start*/
+    $scope.model = MovieService.getMovies();
+}]);
+;angular.module("moviedb").service("MovieService", [function() {
+
+    this.getMovies = function() {
+        return [{
+            "title": "Deadpool",
+            "poster_url": "https://image.tmdb.org/t/p/w185/inVq3FRqcYIRl2la8iZikYYxFNR.jpg",
+            "rating": 7.2,
+            "release_date": "2016-02-19"
+        }, {
+            "title": "The Hunger Games: Mockingjay - Part 2",
+            "poster_url": "https://image.tmdb.org/t/p/w185/nN4cEJMHJHbJBsp3vvvhtNWLGqg.jpg",
+            "rating": 6.8,
+            "release_date": "2015-11-27"
+        }, {
+            "title": "Batman v Superman: Dawn of Justice",
+            "poster_url": "https://image.tmdb.org/t/p/w185/6bCplVkhowCjTHXWv49UjRPn0eK.jpg",
+            "rating": 5.1,
+            "release_date": "2016-03-23"
+        }, {
+            "title": "Mad Max: Fury Road",
+            "poster_url": "https://image.tmdb.org/t/p/w185/kqjL17yufvn9OVLyXYpvtyrFfak.jpg",
+            "rating": 7.4,
+            "release_date": "2015-05-15"
+        }, {
+            "title": "Zootopia",
+            "poster_url": "https://image.tmdb.org/t/p/w185/sM33SANp9z6rXW8Itn7NnG1GOEs.jpg",
+            "rating": 7.4,
+            "release_date": "2016-02-12"
+        }];
+    };
 }]);
