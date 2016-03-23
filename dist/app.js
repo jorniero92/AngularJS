632ae4c3d79b0f42878b8cb3903e7cdeb975376b
jorge nieto <jorge.nieto@babel.es>
2016-03-23 09:34:34 +0100

    Formato bien puesto, todo listo para que funcionen bien la peticiones

--- dist/app.js
+++ dist/app.js
@@ -31710,10 +31710,28 @@ angular.module("moviedb").controller("MenuController", ["$scope","$location", fu
 
     );
 }]);
-;angular.module("moviedb").service("MovieService", ["$http", function($http) {
+;angular.module("moviedb").service("MovieService", ["$http", "$q", function($http, $q) {
 
     this.getMovies = function() {
 
-        return $http.get('/api/movies/');
+
+        // Crear el objeto diferido
+        var deffered = $q.defer();
+
+        // hacer asincrono el trabajo
+        $http.get('/api/movies/').then(
+            //peticion ok
+            function(response) {
+                //resolver la promesa
+                deffered.resolve(response.data);
+            },
+            //peticion KO
+            function(response) {
+                //rechazar la promesa
+                deffered.resolve(response.data);
+            }
+        );
+        return deffered.promise;
+
     };
 }]);
