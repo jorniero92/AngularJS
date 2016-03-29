780a989fe5f2c42787b5777fd45bd7d5a80ba49d
jorge nieto <jorge.nieto@babel.es>
2016-03-29 13:07:50 +0200

    formulario con borrado al final del formulario y añadido bien

--- scripts/services/APIClient.js
+++ scripts/services/APIClient.js
@@ -42,5 +42,26 @@ angular.module("moviedb").service("APIClient", ["$http", "$q", "apiPaths", "URL"
         return this.apiRequest(url);
     };
 
+    /*crear una nueva pelicula, hacer un post a la URL */
+    this.createMovie = function(movie) {
+        // Crear el objeto diferido
+        var deffered = $q.defer();
+
+        // hacer asincrono el trabajo
+        $http.post(apiPaths.movies, movie).then(
+            //peticion ok
+            function(response) {
+                //resolver la promesa
+                deffered.resolve(response.data);
+            },
+            //peticion KO
+            function(response) {
+                //rechazar la promesa
+                deffered.reject(response.data);
+            }
+        );
+        //devolver la promesa
+        return deffered.promise;
+    };
 
 }]);
